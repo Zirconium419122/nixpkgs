8261a5636f099818748f92e36d6cbcba587984e4
Zirconium419122 <rasmus@liaskar.net>
2026-05-21 20:59:17 +0200

    Refactor wrapping in openfoam; wrap executables in platforms directory

--- pkgs/by-name/op/openfoam/package.nix
+++ pkgs/by-name/op/openfoam/package.nix
@@ -22,23 +22,39 @@ stdenv.mkDerivation (attr: {
   installPhase = ''
     OUT_DIR="$src/opt/OpenFOAM-${openfoam-unwrapped.version}"
 
+    ls -lh "$OUT_DIR"
+    ls -lh "$OUT_DIR/platforms/linux64GccDPInt32Opt/bin"
+
+    wrapDir() {
+      local dir="$1"
+
+      for p in "$dir"/*; do
+        [ -f "$p" ] || continue
+        [ -x "$p" ] || continue
+
+        local name
+        name="$(basename "$p")"
+
+        makeWrapper "$p" "$out/bin/$name" \
+          --run "
+            set +eu
+
+            export PATH='${lib.makeBinPath [
+              flex
+              gnumake
+              paraview
+            ]}:$PATH'
+
+            source '$OUT_DIR/etc/bashrc'
+
+            export MPI_ARCH_PATH='${openmpi}'
+          "
+      done
+    }
+
     mkdir -p "$out/bin"
-    for p in "$OUT_DIR"/bin/*; do
-      [ -f "$p" ] || continue
-      [ -x "$p" ] || continue
 
-      name="$(basename "$p")"
-
-      makeWrapper "$p" "$out/bin/$name" \
-        --run "
-          set +eu
-
-          export PATH='${lib.makeBinPath [ flex gnumake paraview ]}:$PATH'
-
-          source '$OUT_DIR/etc/bashrc'
-
-          export MPI_ARCH_PATH=\"${openmpi}\"
-        "
-    done
+    wrapDir "$OUT_DIR/bin"
+    wrapDir "$OUT_DIR/platforms/linux64GccDPInt32Opt/bin"
   '';
 })
